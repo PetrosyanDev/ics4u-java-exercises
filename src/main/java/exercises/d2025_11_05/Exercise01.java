f06df014b760e839ec446bc05ffba9356490f086
ErikDev <dev.erikpetrosyan@gmail.com>
2025-11-06 10:32:05 -0500

    feat: 1105

---
+++ src/main/java/exercises/d2025_11_05/Exercise01.java
@@ -0,0 +1,13 @@
+/*
+ * Student: © Erik Petrosyan
+ * Date: 11/05/2025
+ */
+
+package exercises.d2025_11_05;
+
+public class Exercise01 {
+    public static void main(String[] args) {
+        System.out.println("Exercise01 - 11/05/2025");
+
+    }
+}
