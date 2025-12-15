0f370a86d2db38bb3d9fd53f7816618005b0ac68
ErikDev <dev.erikpetrosyan@gmail.com>
2025-12-15 10:45:50 -0500

    fix: constructor overloading

--- src/main/java/assignments/d2025_12_15/Exercise01.java
+++ src/main/java/assignments/d2025_12_15/Exercise01.java
@@ -16,6 +16,18 @@ class Car {
         this.year = 0;
     }
 
+    public Car(String make) {
+        this.make = make;
+        this.model = "";
+        this.year = 0;
+    }
+
+    public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        this.year = 0;
+    }
+
     public Car(String make, String model, int year) {
         this.make = make;
         this.model = model;
